55542a31ba90ea8098d749a0294e308ca7154535
Valdyn Hunt <valdyn@mac.com>
2019-03-11 11:28:15 -0700

    add options to lightGallery

--- js/scripts.js
+++ js/scripts.js
@@ -3,4 +3,13 @@ console.log('OK');
 
 
 // Light Gallery
-$('#gallery-1').lightGallery();
+$('#gallery-1').lightGallery({
+    thumbnail: false,
+    share: false,
+    download: false
+});
+
+$('#gallery-2').lightGallery({
+    share: false,
+    download: false
+});
