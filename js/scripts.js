02af54e95398cc0ffd4d1301aa0423211d5af03b
Valdyn Hunt <valdyn@mac.com>
2019-03-11 11:47:46 -0700

    modify lightbar options

--- js/scripts.js
+++ js/scripts.js
@@ -6,10 +6,14 @@ console.log('OK');
 $('#gallery-1').lightGallery({
     thumbnail: false,
     share: false,
-    download: false
+    download: false,
+    mode: 'lg-tube',
+    speed: 300
 });
 
 $('#gallery-2').lightGallery({
     share: false,
-    download: false
+    download: false,
+    mode: 'lg-scale-up',
+    speed: 2000
 });
