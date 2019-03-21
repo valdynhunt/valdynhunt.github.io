9860d8abbd574ee47313f6e037bdfd0435df4f00
Valdyn Hunt <valdyn@mac.com>
2019-03-20 20:58:55 -0700

    modify lightbox

--- js/scripts.js
+++ js/scripts.js
@@ -18,31 +18,70 @@ $('#gallery-2').lightGallery({
     speed: 2000
 });
 
+$('#gallery-3').lightGallery({
+  thumbnail: false,
+  share: false,
+  download: false,
+  mode: 'lg-tube',
+  speed: 300
+});
+
+$('#gallery-4').lightGallery({
+  thumbnail: false,
+  share: false,
+  download: false,
+  mode: 'lg-tube',
+  speed: 300
+});
+
+$('#gallery-5').lightGallery({
+  thumbnail: false,
+  share: false,
+  download: false,
+  mode: 'lg-tube',
+  speed: 300
+});
+
+$('#gallery-6').lightGallery({
+  thumbnail: false,
+  share: false,
+  download: false,
+  mode: 'lg-tube',
+  speed: 300
+});
+
+$('#gallery-7').lightGallery({
+  thumbnail: false,
+  share: false,
+  download: false,
+  mode: 'lg-tube',
+  speed: 300
+});
 
 
 
 // Controls for Views
 $('#grid').click( function(){
   
-    // Remove List
+    // Remove List and add Grid
   $('main').removeClass('list');
-  
   $('main').addClass('grid');
   // console.log('Grid Click');
-
 });
 
 $('#list').click( function(){
   
-  // Remove Grid
+  // Remove Grid and add Class
   $('main').removeClass('grid');
-  
   $('main').addClass('list');
   // console.log('List Click');
-  
 });
 
 // Grid View
 
 
 // List View
+
+
+// nav color
+$('#nav li:hover').addClass("navColor");
