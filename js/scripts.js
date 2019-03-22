ad8da52b7cc384f8abdc96a74484c7c168b3e92b
Valdyn Hunt <valdyn@mac.com>
2019-03-21 21:53:36 -0700

    add portfolio filter

--- js/scripts.js
+++ js/scripts.js
@@ -80,3 +80,48 @@ $('#list').click( function(){
 
 
 // List View
+
+
+
+var checkClass = function() {
+  
+  // Remove Existing Hide 
+  if ( $('img').hasClass('hide') ) {
+    $('img').removeClass('hide'); 
+  }
+  
+};
+
+// Click Photography
+$('#photo').click( function() {
+  
+  // Check Class
+  checkClass();
+  
+  // Hide Other Stuff
+  $('img:not(.photo)').toggleClass('hide');
+
+});
+
+// Click Illustration
+$('#illus').click( function() {
+  
+    checkClass();
+    $('img:not(.illus)').toggleClass('hide');
+  
+});
+
+// Click Logo
+$('#logo').click( function() {
+ 
+    checkClass();
+    $('img:not(.logo)').toggleClass('hide');
+  
+});
+
+// Show All 
+$('#all').click( function() {
+  
+  checkClass();
+  
+});
